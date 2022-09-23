5a5ca1ef926f4d4f6295cd8b3bc67a55dcd52ddd
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-09-23 12:56:05 +0300

    edit profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,28 @@
 export const Profile = () => {
   return (
-    <div class="profile">
-      <div class="description">
+    <div>
+      <div>
         <img
           src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
           alt="User avatar"
-          class="avatar"
         />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <p>Petra Marica</p>
+        <p>@pmarica</p>
+        <p>Salvador, Brasil</p>
       </div>
 
-      <ul class="stats">
+      <ul>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span>Followers</span>
+          <span>1000</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span>Views</span>
+          <span>2000</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span>Likes</span>
+          <span>3000</span>
         </li>
       </ul>
     </div>
