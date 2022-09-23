398d9ebc27086808948a8db508e47d3f97ad17b2
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-09-23 13:15:46 +0300

    npm i styled-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './Friends/FriendList';
-import { FriendListItem } from './Friends/FriendListItem';
-import friends from '../components/Friends/friends.json';
+// import { Statistics } from './Statistics/Statistics';
+// import { FriendList } from './Friends/FriendList';
+// import { FriendListItem } from './Friends/FriendListItem';
+// import friends from '../components/Friends/friends.json';
 
 export const App = () => {
   return (
