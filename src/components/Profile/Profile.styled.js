b6e27fd52b819ed193d8890e38edd84397e977a2
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-09-25 15:07:44 +0300

    add style profile

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: #fdfdfe;
-  padding: 20px;
+
+  background-color: #d4d5d8;
+  padding: 50px;
   width: fit-content;
   margin: 0 auto;
-  border-radius: 20px;
 `;
 
 export const CardTop = styled.div`
@@ -16,44 +16,54 @@ export const CardTop = styled.div`
 
   justify-content: center;
   align-items: center;
+
+  background-color: #fdfdfe;
+  padding-top: 40px;
+  padding-bottom: 20px;
+  border: 1px solid silver;
+  border-radius: 4px 4px 0 0;
 `;
 
 export const Avatar = styled.img`
-  border-radius: 10px;
+  width: 100px;
+  height: 100px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
 `;
 
 export const Name = styled.p`
-  padding: 20px;
+  padding: 5px;
 `;
 
 export const Tag = styled.p`
-  padding: 20px;
+  color: #a4a6a8;
+  padding: 5px;
 `;
 
 export const Location = styled.p`
-  padding: 20px;
+  color: #a4a6a8;
+  padding: 5px;
 `;
 
 export const CardBottom = styled.ul`
   display: flex;
   border: solid 2px;
+  margin-top: 15px;
 `;
 export const BlockInfo = styled.li`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   border: solid 1px;
   width: 100%;
-
-  flex-direction: column;
-  background-color: #d4d5d8;
-
-  /* padding: 20px; */
 `;
 
 export const InfoName = styled.span`
-  color: #a4a6a8;
-  padding: 20px;
+  margin-bottom: 10px;
+  padding: 0px 10px;
 `;
 export const InfoValue = styled.span`
   color: #102445;
-  padding: 20px;
 `;
